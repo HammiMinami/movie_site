595e9b37e6cd2b186eaf04f0547da113c226390f
HammiMinami <christineadri@gmail.com>
2023-11-28 04:14:14 +0800

    Updates

---
+++ precache-manifest.1f69800ed3d152e5ef93f27adf1d5936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f382e297bbd4da4c7e81dc291597dd2b",
+    "url": "/movie_site/index.html"
+  },
+  {
+    "revision": "dc7df662252b7370f284",
+    "url": "/movie_site/static/css/main.66b0be5b.chunk.css"
+  },
+  {
+    "revision": "f9a600ffcb67ffcb1904",
+    "url": "/movie_site/static/js/2.d73db9ac.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_site/static/js/2.d73db9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7df662252b7370f284",
+    "url": "/movie_site/static/js/main.4cf43218.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie_site/static/js/main.4cf43218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee4b85a7505a5aca919",
+    "url": "/movie_site/static/js/runtime-main.664e63d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.234bed5cea08997fe80c35874506e6b8.js"
+  "/movie_site/precache-manifest.1f69800ed3d152e5ef93f27adf1d5936.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
