00e3cb66938c09d44a49c3d97024bda7da7334e6
HammiMinami <christineadri@gmail.com>
2023-11-28 03:26:22 +0800

    Updates

---
+++ precache-manifest.234bed5cea08997fe80c35874506e6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a55bf9994ffe8f50a6c795be224835f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7df662252b7370f284",
+    "url": "/static/css/main.66b0be5b.chunk.css"
+  },
+  {
+    "revision": "f9a600ffcb67ffcb1904",
+    "url": "/static/js/2.d73db9ac.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d73db9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7df662252b7370f284",
+    "url": "/static/js/main.4cf43218.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.4cf43218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a35b162100f2ba26a2",
+    "url": "/static/js/runtime-main.c53d5ad8.js"
+  }
+]);
